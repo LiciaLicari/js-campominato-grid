23f237ea79d008b3b732856f179d9ef7b161c142
“LiciaLicari” <licianbsplicari@gmail.com>
2023-09-01 15:47:17 +0200

    add function

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,30 +2,26 @@ console.log("miao");
 
 /*
 Consegna
-L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
-Ogni cella ha un numero progressivo, da 1 a 100.
-Ci saranno quindi 10 caselle per ognuna delle 10 righe.
-Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro 
-ed emetto un messaggio in console con il numero della cella cliccata.
-Consigli del giorno: :party_wizard:
-Scriviamo prima cosa vogliamo fare passo passo in italiano, 
-dividiamo il lavoro in micro problemi.
-Ad esempio:
-Di cosa ho bisogno per generare i numeri?
-Proviamo sempre prima con dei console.log() 
-per capire se stiamo ricevendo i dati giusti.
-Le validazioni e i controlli possiamo farli anche in un secondo momento.
+- L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
+- Ogni cella ha un numero progressivo, da 1 a 100 (Ci saranno quindi 10 caselle per ognuna delle 10 righe)
+- Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro 
+- emetto un messaggio in console con il numero della cella cliccata.
 */
 
 
 const fieldEl = document.querySelector(".field");
 const limit = 100 // numero delle celle
+spawnField(fieldEl, limit);
 
-//creo il campo
 
-for (let i = 0; i < limit; i++){
-    const cellEl = document.createElement("div");
-    cellEl.classList = "cell";
-    cellEl.innerHTML = i + 1;
-    fieldEl.append(cellEl)
+function spawnField(domEl, limit) {
+    //creo il campo
+    
+    for (let i = 0; i < limit; i++){
+        const cellEl = document.createElement("div");
+        cellEl.classList = "cell";
+        cellEl.innerHTML = i + 1;
+        fieldEl.append(cellEl)
+    }
+
 }
